1c89660b2a0d703f197ce2658ef79762188a68d7
waynetian-42 <arthassum@gmail.com>
2024-02-15 20:59:20 +0800

    角色移动时旋转面朝方向

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -30,8 +30,12 @@ public class Player : MonoBehaviour
         // 输入向量归一化
         inputVector = inputVector.normalized;
 
-        Vector3 moveVector = new Vector3(inputVector.x, 0, inputVector.y);
-        // 注意运算顺序
-        transform.position += moveSpeed * Time.deltaTime * moveVector;
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        // 注意运算顺序，先进行标量乘法，再进行矢量乘法
+        transform.position += moveSpeed * Time.deltaTime * moveDir;
+
+        float rotationSpeed = 10f;
+        // 采用插值平滑改变角色面朝方向
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, rotationSpeed * Time.deltaTime);
     }
 }
