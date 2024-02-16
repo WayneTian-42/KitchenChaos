5f8e0c8e3c14d6fed37a7bc1c78a29ceb40a4377
waynetian-42 <arthassum@gmail.com>
2024-02-16 18:16:13 +0800

    添加交互基础框架

---
+++ Assets/Scripts/ClearCounter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearCounter : MonoBehaviour
+{
+    /// <summary>
+    /// 交互函数
+    /// </summary>
+    public void Interact()
+    {
+        Debug.Log("Interact");
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
+using UnityEngine.Video;
 
 public class Player : MonoBehaviour
 {
@@ -8,10 +10,34 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed = 7f;
     // 输入处理
     [SerializeField] private GameInput gameInput;
+    // 计数器类的图层
+    [SerializeField] private LayerMask counterLayerMask;
     // 角色是否在移动
     private bool isWalking;
 
+    // 记录上次交互方向
+    private Vector3 lastInteractDir;
+
     private void Update()
+    {
+        HandleMovement();
+        HandleInteraction();
+    }
+
+    /// <summary>
+    /// 角色是否在移动
+    /// </summary>
+    /// <returns>true表示角色在移动，false表示不在移动</returns>
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+
+    /// <summary>
+    /// 处理角色移动
+    /// </summary>
+    private void HandleMovement()
     {
         Vector2 inputVector = gameInput.GetInputVectorNormalized();
 
@@ -73,11 +99,30 @@ public class Player : MonoBehaviour
     }
 
     /// <summary>
-    /// 角色是否在移动
+    /// 交互处理
     /// </summary>
-    /// <returns>true表示角色在移动，false表示不在移动</returns>
-    public bool IsWalking()
+    private void HandleInteraction()
     {
-        return isWalking;
+        Vector2 inputVector = gameInput.GetInputVectorNormalized();
+
+        // 移动方向
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        // 记录交互方向
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+
+        float interactDis = 2f;
+        // 碰撞检测，添加图层
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDis, counterLayerMask))
+        {
+            // 尝试获取ClearCounter
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+        }
     }
 }
