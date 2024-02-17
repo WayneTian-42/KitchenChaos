87f0933767dbff61b1533955902b530cc930ec79
waynetian-42 <arthassum@gmail.com>
2024-02-17 18:05:39 +0800

    实现柜台和物品绑定；完成逻辑绑定以及视觉效果

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -9,6 +9,20 @@ public class ClearCounter : MonoBehaviour
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     // 柜台顶部中心位置
     [SerializeField] private Transform counterTopPoint;
+    // 柜台的物品
+    private KitchenObject kitchenObject;
+
+    // 测试用
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing;
+
+    private void Update()
+    {
+        if (testing && kitchenObject != null && Input.GetKeyDown(KeyCode.T))
+        {
+            kitchenObject.SetClearCounter(secondClearCounter);
+        }
+    }
 
     /// <summary>
     /// 交互函数，用于生成厨房中指定的object
@@ -16,12 +30,72 @@ public class ClearCounter : MonoBehaviour
     public void Interact()
     {
         // Debug.Log("Interact");
-        // 在counterTopPoint位置复制一个scriptable object出来
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.GetObjectPrefab(), counterTopPoint);
-        // 修改局部位置为全零，防止出现误差？
-        kitchenObjectTransform.localPosition = Vector3.zero;
+        // 只有当物品为空时才能交互，保证最多只存在一个物品
+        if (kitchenObject == null)
+        {
+            // 在counterTopPoint位置复制一个scriptable object出来
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.GetObjectPrefab(), counterTopPoint);
 
-        // 获取生成物体的KitchenObject类，再获取其对应的scriptable object，得到生成物体的名字
-        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().GetObjectName());
+            // 直接更新物品对应的柜台，同时实现逻辑操作以及视觉更改
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+            
+            // 获取生成物体的KitchenObject类，再获取其对应的scriptable object，得到生成物体的名字
+            // Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().GetObjectName());
+        }
+        else
+        {
+            Debug.Log(kitchenObject);
+        }
+    }
+
+    /// <summary>
+    /// 获取柜台顶部中心位置
+    /// </summary>
+    /// <returns>柜台顶部中心位置</returns>
+    public Transform GetCounterTopPoint()
+    {
+        return counterTopPoint;
+    }
+
+    /// <summary>
+    /// 设置柜台的物品
+    /// </summary>
+    /// <param name="newKitchenObject">物品</param>
+    public void SetKitchenObject(KitchenObject newKitchenObject)
+    {
+        if (kitchenObject == null)
+        {
+            kitchenObject = newKitchenObject;
+        }
+        else
+        {
+            Debug.LogError("Counter already had a kitchen object!");
+        }
+    }
+
+    /// <summary>
+    /// 获取柜台的物品
+    /// </summary>
+    /// <returns>柜台的物品</returns>
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    /// <summary>
+    /// 清空柜台物品
+    /// </summary>
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    /// <summary>
+    /// 判断柜台是否含有物品
+    /// </summary>
+    /// <returns>true表示含有物品，false表示不含有物品</returns>
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
     }
 }
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -7,6 +7,8 @@ public class KitchenObject : MonoBehaviour
     // 厨房预制体scriptable object
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
+    // 该object所在的counter
+    private ClearCounter clearCounter;
     /// <summary>
     /// 获取prefab对应的scriptable object
     /// </summary>
@@ -14,5 +16,43 @@ public class KitchenObject : MonoBehaviour
     public KitchenObjectSO GetKitchenObjectSO()
     {
         return kitchenObjectSO;
+    }
+
+    /// <summary>
+    /// 设置物品对应的新柜台并修改逻辑以及视觉效果
+    /// </summary>
+    /// <param name="newClearCounter">物品放置的新柜台</param>
+    public void SetClearCounter(ClearCounter newClearCounter)
+    {
+        // 修改柜台和物品的绑定逻辑
+        // 清空当前柜台
+        if (clearCounter != null)
+        {
+            clearCounter.ClearKitchenObject();
+        }
+
+        // 如果新柜台已经具有物品，则报错
+        if (newClearCounter.HasKitchenObject())
+        {
+            Debug.LogError("Counter already had a kitchen object!");
+        }
+
+        // 更新柜台
+        clearCounter = newClearCounter;
+        // 更新柜台的物品
+        clearCounter.SetKitchenObject(this);
+
+        // 直接修改物品的父级对象，相当于同时移动在Hierarchy中的逻辑结构以及视觉效果上的物理位置
+        transform.parent = clearCounter.GetCounterTopPoint();
+        transform.localPosition = Vector3.zero;
+    }
+
+    /// <summary>
+    /// 获取物品对应的柜台
+    /// </summary>
+    /// <returns>物品对应的柜台</returns>
+    public ClearCounter GetClearCounter()
+    {
+        return clearCounter;
     }
 }
